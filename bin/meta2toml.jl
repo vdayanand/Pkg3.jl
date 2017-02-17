7ef05a4278be43eafc85b4951a11e76468f94148
Stefan Karpinski <stefan@karpinski.org>
2017-02-17 18:34:49 -0500

    meta2toml.jl wip

--- bin/meta2toml.jl
+++ bin/meta2toml.jl
@@ -1,8 +1,11 @@
 #!/usr/bin/env julia
 
+using Base.Random: UUID
+using Base.Pkg.Types
+using Base.Pkg.Reqs: Reqs, Requirement
+
 using SHA
 # using TOML # not the registered one, but https://github.com/StefanKarpinski/TOML.jl
-using Base.Random: UUID
 
 function uuid5(namespace::UUID, key::String)
     data = [reinterpret(UInt8, [namespace.value]); Vector{UInt8}(key)]
@@ -25,8 +28,9 @@ for (i, pkg) in enumerate(names)
     vers = joinpath(path, "versions")
     isfile(urlf) && isdir(vers) || continue
     versions = sort!(readdir(vers), by=VersionNumber)
+    versions[1] == "0.0.0" && shift!(versions)
     isempty(versions) && continue
-    # we should emit a package entry
+    # emit package entry
     url = readchomp(urlf)
     uuid = uuid5(uuid_julia, pkg)
     println("""
@@ -35,14 +39,36 @@ for (i, pkg) in enumerate(names)
     repository = "$url"
     """)
     for (j, v) in enumerate(versions)
+        # emit version info
         verd = joinpath(vers, v)
         sha1f = joinpath(verd, "sha1")
         isfile(sha1f) || continue
         sha1 = readchomp(sha1f)
+        reqf = joinpath(verd, "requires")
+        reqs = Reqs.read(reqf)
+        julias = mapreduce(
+            r->r.versions,
+            (a,b)->intersect(a,b),
+            VersionSet(),
+            filter(r->isa(r,Requirement) && r.package == "julia", reqs)
+        )
+        @assert length(julias.intervals) == 1
+        julia = julias.intervals[1]
+        jlo = Base.thisminor(max(julia.lower, v"0.1"))
+        jhi = Base.thisminor(min(julia.upper, v"0.5"))
+        jver = jlo < jhi ?
+            "$(jlo.major).$(jlo.minor)-$(jhi.major).$(jhi.minor)" :
+            "$(jlo.major).$(jlo.minor)"
         println("""
             [[$pkg.version]]
             version = "$v"
+            julia = "$jver"
             SHA1 = "$sha1"
         """)
+        # emit compatibility info
+
+        # println(STDOUT, reqs)
+        # break
     end
+    # break
 end

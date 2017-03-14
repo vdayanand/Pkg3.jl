e424992a574292cac5ddc219d55607db75d2fb93
Stefan Karpinski <stefan@karpinski.org>
2017-03-14 13:24:50 -0400

    treat "julia" as a normal dependency

--- bin/meta2toml.jl
+++ bin/meta2toml.jl
@@ -52,7 +52,6 @@ end
 
 struct Version
     sha1::String
-    julia::VersionInterval
     requires::Dict{String,Require}
 end
 
@@ -61,6 +60,9 @@ struct Package
     url::String
     versions::Dict{VersionNumber,Version}
 end
+
+Require(versions::VersionInterval) = Require(versions, Symbol[])
+Version(sha1::String) = Version(sha1, Dict{String,Require}())
 
 function load_requires(path::String)
     requires = Dict{String,Require}()
@@ -86,9 +88,7 @@ function load_versions(dir::String)
         sha1 = joinpath(path, "sha1")
         isfile(sha1) || continue
         requires = load_requires(joinpath(path, "requires"))
-        julia = pop!(requires, "julia", Require(VersionInterval(v"0.1", v"0.6"), []))
-        @assert isempty(julia.systems)
-        versions[VersionNumber(ver)] = Version(readchomp(sha1), julia.versions, requires)
+        versions[VersionNumber(ver)] = Version(readchomp(sha1), requires)
     end
     return versions
 end
@@ -100,8 +100,45 @@ function load_packages(dir::String)
         url = joinpath(path, "url")
         versions = joinpath(path, "versions")
         isfile(url) || continue
-        packages[pkg] = Package(uuid5(uuid_julia, pkg), readchomp(url), load_versions(versions))
+        packages[pkg] = Package(
+            uuid5(uuid_julia, pkg),
+            readchomp(url),
+            load_versions(versions),
+        )
     end
+    packages["julia"] = Package(
+        uuid5(uuid_julia, "julia"),
+        "https://github.com/JuliaLang/julia.git",
+        Dict(
+            v"0.1.1"  => Version("bddb70374fdbb2256782398c13430fa4d8b55d0b"),
+            v"0.1.2"  => Version("1187040d027210cc466f0b6a6d54118fd692cf2d"),
+            v"0.2.0"  => Version("68e4f269049b3a77eee1e185e619c3513034b3a6"),
+            v"0.2.1"  => Version("7c5dea64f55afb7d9a7224a337d3c0bd5a707c50"),
+            v"0.3.0"  => Version("768187890c2709bf2ff06818f40e1cdc79bd44b0"),
+            v"0.3.1"  => Version("c03f413bbdb46c00033f4eaad402995cfe3b7be5"),
+            v"0.3.10" => Version("c8ceeefcc1dc25953a644622a895a3adcbc80dad"),
+            v"0.3.11" => Version("483dbf5279efe29f94cf35d42de0ab0a673fb34e"),
+            v"0.3.12" => Version("80aa77986ec84067f4696439f10fc77c93703bc7"),
+            v"0.3.2"  => Version("8227746b95146c2921f83d2ae5f37ecd146592d8"),
+            v"0.3.3"  => Version("b24213b893ace6ca601722f6cdaac15f2b034b7b"),
+            v"0.3.4"  => Version("33920266908e950936ef3f8503810a2d47333741"),
+            v"0.3.5"  => Version("a05f87b79ad62beb033817fdfdefa270c9557aaf"),
+            v"0.3.6"  => Version("0c24dca65c031820b91721139f0291068086955c"),
+            v"0.3.7"  => Version("cb9bcae93a32b42cec02585c387396ff11836aed"),
+            v"0.3.8"  => Version("79599ada444fcc63cb08aed64b4ff6a415bb4d29"),
+            v"0.3.9"  => Version("31efe690bea1fd8f4e44692e205fb72d34f50ad3"),
+            v"0.4.0"  => Version("0ff703b40afddf9b705bd6a06d3a59cb4c089ea5"),
+            v"0.4.1"  => Version("cbe1bee3a8f27ba4f349556857bf4615ee5fa68f"),
+            v"0.4.2"  => Version("bb73f3489d837e3339fce2c1aab283d3b2e97a4c"),
+            v"0.4.3"  => Version("a2f713dea5ac6320d8dcf2835ac4a37ea751af05"),
+            v"0.4.4"  => Version("ae683afcc4cc824dcf68251569a6cf8760e4fb6f"),
+            v"0.4.5"  => Version("2ac304dfba75fad148d4070ef4f8a2e400c305bb"),
+            v"0.4.6"  => Version("2e358ce975029ec97aba5994c17d4a2169c3b085"),
+            v"0.4.7"  => Version("ae26b25d43317d7dd3ca05f60b70677aab9c0e08"),
+            v"0.5.0"  => Version("3c9d75391c72d7c32eea75ff187ce77b2d5effc8"),
+            v"0.5.1"  => Version("6445c82d0060dbe82b88436f0f4371a4ee64d918"),
+        ),
+    )
     return packages
 end
 
@@ -117,7 +154,6 @@ function prune!(packages::Associative{String,Package})
         filter!(packages) do pkg, p
             filter!(p.versions) do ver, v
                 @clean ver == thispatch(ver) > v"0.0.0" &&
-                !isempty(julia_versions(v.julia)) &&
                 all(v.requires) do kv
                     req, r = kv
                     haskey(packages, req) &&

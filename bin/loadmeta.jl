acc4200249faa19ac476c6969d05f393607d4f30
Stefan Karpinski <stefan@karpinski.org>
2017-05-05 11:00:03 -0400

    split out loadmeta.jl from meta2toml.jl

---
+++ bin/loadmeta.jl
@@ -0,0 +1,506 @@
+#!/usr/bin/env julia
+
+using Base.Random: UUID
+using Base.Pkg.Types
+using Base.Pkg.Reqs: Reqs, Requirement
+using Base.LinAlg: checksquare
+using Base: thispatch, thisminor, nextpatch, nextminor
+
+using SHA
+using PicoSAT
+
+## Computing UUID5 values from (namespace, key) pairs ##
+
+function uuid5(namespace::UUID, key::String)
+    data = [reinterpret(UInt8, [namespace.value]); Vector{UInt8}(key)]
+    u = reinterpret(UInt128, sha1(data)[1:16])[1]
+    u &= 0xffffffffffff0fff3fffffffffffffff
+    u |= 0x00000000000050008000000000000000
+    return UUID(u)
+end
+uuid5(namespace::UUID, key::AbstractString) = uuid5(namespace, String(key))
+
+const uuid_dns = UUID(0x6ba7b810_9dad_11d1_80b4_00c04fd430c8)
+const uuid_julia = uuid5(uuid_dns, "julialang.org")
+
+## Loading data into various data structures ##
+
+struct Require
+    versions::VersionInterval
+    systems::Vector{Symbol}
+end
+
+struct Version
+    sha1::String
+    requires::Dict{String,Require}
+end
+
+struct Package
+    uuid::UUID
+    url::String
+    versions::Dict{VersionNumber,Version}
+end
+
+Require(versions::VersionInterval) = Require(versions, Symbol[])
+Version(sha1::String) = Version(sha1, Dict{String,Require}())
+
+function load_requires(path::String)
+    requires = Dict{String,Require}()
+    isfile(path) || return requires
+    for r in filter!(r->r isa Requirement, Reqs.read(path))
+        @assert length(r.versions.intervals) == 1
+        new = haskey(requires, r.package)
+        versions, systems = r.versions.intervals[1], r.system
+        if haskey(requires, r.package)
+            versions = versions ∩ requires[r.package].versions
+            systems  = systems  ∪ requires[r.package].systems
+        end
+        requires[r.package] = Require(versions, systems)
+    end
+    return requires
+end
+
+function load_versions(dir::String)
+    versions = Dict{VersionNumber,Version}()
+    isdir(dir) || return versions
+    for ver in readdir(dir)
+        path = joinpath(dir, ver)
+        sha1 = joinpath(path, "sha1")
+        isfile(sha1) || continue
+        requires = load_requires(joinpath(path, "requires"))
+        versions[VersionNumber(ver)] = Version(readchomp(sha1), requires)
+    end
+    return versions
+end
+
+function load_packages(dir::String)
+    packages = Dict{String,Package}()
+    for pkg in readdir(dir)
+        path = joinpath(dir, pkg)
+        url = joinpath(path, "url")
+        versions = joinpath(path, "versions")
+        isfile(url) || continue
+        packages[pkg] = Package(
+            uuid5(uuid_julia, pkg),
+            readchomp(url),
+            load_versions(versions),
+        )
+    end
+    packages["julia"] = Package(
+        uuid5(uuid_julia, "julia"),
+        "https://github.com/JuliaLang/julia.git",
+        Dict(
+            v"0.1.1"  => Version("bddb70374fdbb2256782398c13430fa4d8b55d0b"),
+            v"0.1.2"  => Version("1187040d027210cc466f0b6a6d54118fd692cf2d"),
+            v"0.2.0"  => Version("68e4f269049b3a77eee1e185e619c3513034b3a6"),
+            v"0.2.1"  => Version("7c5dea64f55afb7d9a7224a337d3c0bd5a707c50"),
+            v"0.3.0"  => Version("768187890c2709bf2ff06818f40e1cdc79bd44b0"),
+            v"0.3.1"  => Version("c03f413bbdb46c00033f4eaad402995cfe3b7be5"),
+            v"0.3.10" => Version("c8ceeefcc1dc25953a644622a895a3adcbc80dad"),
+            v"0.3.11" => Version("483dbf5279efe29f94cf35d42de0ab0a673fb34e"),
+            v"0.3.12" => Version("80aa77986ec84067f4696439f10fc77c93703bc7"),
+            v"0.3.2"  => Version("8227746b95146c2921f83d2ae5f37ecd146592d8"),
+            v"0.3.3"  => Version("b24213b893ace6ca601722f6cdaac15f2b034b7b"),
+            v"0.3.4"  => Version("33920266908e950936ef3f8503810a2d47333741"),
+            v"0.3.5"  => Version("a05f87b79ad62beb033817fdfdefa270c9557aaf"),
+            v"0.3.6"  => Version("0c24dca65c031820b91721139f0291068086955c"),
+            v"0.3.7"  => Version("cb9bcae93a32b42cec02585c387396ff11836aed"),
+            v"0.3.8"  => Version("79599ada444fcc63cb08aed64b4ff6a415bb4d29"),
+            v"0.3.9"  => Version("31efe690bea1fd8f4e44692e205fb72d34f50ad3"),
+            v"0.4.0"  => Version("0ff703b40afddf9b705bd6a06d3a59cb4c089ea5"),
+            v"0.4.1"  => Version("cbe1bee3a8f27ba4f349556857bf4615ee5fa68f"),
+            v"0.4.2"  => Version("bb73f3489d837e3339fce2c1aab283d3b2e97a4c"),
+            v"0.4.3"  => Version("a2f713dea5ac6320d8dcf2835ac4a37ea751af05"),
+            v"0.4.4"  => Version("ae683afcc4cc824dcf68251569a6cf8760e4fb6f"),
+            v"0.4.5"  => Version("2ac304dfba75fad148d4070ef4f8a2e400c305bb"),
+            v"0.4.6"  => Version("2e358ce975029ec97aba5994c17d4a2169c3b085"),
+            v"0.4.7"  => Version("ae26b25d43317d7dd3ca05f60b70677aab9c0e08"),
+            v"0.5.0"  => Version("3c9d75391c72d7c32eea75ff187ce77b2d5effc8"),
+            v"0.5.1"  => Version("6445c82d0060dbe82b88436f0f4371a4ee64d918"),
+        ),
+    )
+    return packages
+end
+
+@eval julia_versions() = $([VersionNumber(0,m) for m=1:5])
+julia_versions(f::Function) = filter(f, julia_versions())
+julia_versions(vi::VersionInterval) = julia_versions(v->v in vi)
+
+macro clean(ex) :(x = $(esc(ex)); $(esc(:clean)) &= x; x) end
+
+function prune!(packages::Associative{String,Package})
+    # remove mutual dependencies
+    for (pkg, dep) in [("Anasol", "Mads"),
+                       ("BIGUQ", "Mads"),
+                       ("Graphics", "Colors"),
+                       ("AWSLambda", "AWSSNS"),
+                       ("URIParser", "HttpCommon")]
+        p = packages[pkg]
+        for (ver, v) in p.versions
+            delete!(v.requires, dep)
+        end
+    end
+    # remove unsatisfiable versions
+    while true
+        clean = true
+        filter!(packages) do pkg, p
+            filter!(p.versions) do ver, v
+                @clean ver == thispatch(ver) > v"0.0.0" &&
+                all(v.requires) do kv
+                    req, r = kv
+                    haskey(packages, req) &&
+                    any(w->w in r.versions, keys(packages[req].versions))
+                end
+            end
+            @clean !isempty(p.versions)
+        end
+        clean && return packages
+    end
+end
+
+## Load package data ##
+
+const dir = length(ARGS) >= 1 ? ARGS[1] : Pkg.dir("METADATA")
+const pkgs = load_packages(dir)
+prune!(pkgs)
+const packages = sort!(collect(keys(pkgs)), by=lowercase)
+
+function representative_versions(pkg::String; packages=Main.pkgs)
+    p = packages[pkg]
+    versions = sort!(collect(keys(p.versions)))
+    rep = pop!(versions)
+    reps = [rep]
+    while !isempty(versions)
+        rep′ = pop!(versions)
+        for (pkg′, p′) in packages, (ver, v) in p′.versions
+            haskey(v.requires, pkg) || continue
+            vers = v.requires[pkg].versions
+            if (rep in vers) ⊻ (rep′ in vers)
+                push!(reps, rep′)
+                rep = rep′
+                break
+            end
+        end
+    end
+    return reverse!(reps)
+end
+
+const versions = Tuple{String,VersionNumber}[]
+for pkg in packages, ver in representative_versions(pkg)
+    push!(versions, (pkg, ver))
+end
+
+const versions_rev = Dict(v => i for (i, v) in enumerate(versions))
+const packages_rev = Dict(p => i for (i, p) in enumerate(packages))
+const m, n = length(pkgs), length(versions)
+
+## Some computational utility functions ##
+
+function package_map()
+    pkgs = zeros(Int, n)
+    for (i, p1) in enumerate(packages),
+        (j, (p2, _)) in enumerate(versions)
+        if p1 == p2
+            pkgs[j] = i
+        end
+    end
+    return pkgs
+end
+
+function requires_map()
+    ind = Dict(p => i for (i, p) in enumerate(packages))
+    [sort!([ind[dep] for dep in keys(pkgs[pkg].versions[ver].requires)])
+                     for (pkg, ver) in versions]
+end
+
+function requires_reverse_map()
+    rev = [Int[] for _ = 1:m]
+    for v in 1:n
+        for req in req_map[v]
+            push!(rev[req], v)
+        end
+    end
+    return rev
+end
+
+function package_versions()
+    los = zeros(Int, m)
+    his = zeros(Int, m)
+    for v in 1:n
+        p = pkg_map[v]
+        los[p] == 0 && (los[p] = v)
+        his[p] = v
+    end
+    [lo:hi for (lo,hi) in zip(los,his)]
+end
+
+function incompatibility_matrix()
+    X = spzeros(Int, n, n)
+    for (i, (p1, v1)) in enumerate(versions)
+        r = pkgs[p1].versions[v1].requires
+        for (j, (p2, v2)) in enumerate(versions)
+            if haskey(r, p2) && v2 ∉ r[p2].versions
+                X[i,j] = X[j,i] = 1
+            end
+        end
+    end
+    return X
+end
+
+function iterate_dependencies(X, P, R; verbose=false)
+    X = max.(0, X - I) # make each node not its own neighbor
+    D = max.(0, min.(1, P'R + I) .- X)
+    for i = 1:typemax(Int)
+        n = nnz(D)
+        verbose && println("Density $i: $(n/length(D))")
+        D .= max.(0, min.(1, D^2) .- X)
+        nnz(D) <= n && break
+    end
+    return D
+end
+
+const pkg_map = package_map()
+const req_map = requires_map()
+const req_rev =  requires_reverse_map()
+const pkg_vers = package_versions()
+
+function is_satisfied(vers::Vector{Int})
+    provided = unique(pkg_map[v] for v in vers)
+    required = unique(r for v in vers for r in req_map[v])
+    required ⊆ provided
+end
+
+function build_cnf!(X::AbstractMatrix, cnf::Vector{Vector{Int}})
+    for (pkg, vers) in enumerate(pkg_vers)
+        push!(cnf, [-(n+pkg); vers])
+        for ver in vers
+            push!(cnf, [-ver, n+pkg])
+        end
+    end
+    for (ver, reqs) in enumerate(req_map), req in reqs
+        push!(cnf, [-ver, n+req])
+    end
+    for (v1, v2) in zip(findn(X)...)
+        push!(cnf, [-v1, -v2])
+    end
+    return cnf
+end
+build_cnf(X::AbstractMatrix) = build_cnf!(X, Vector{Int}[])
+
+function deep_requirements!(req_map, P = Main.P, R = Main.R, X = Main.X)
+    D = min.(1, P'R)
+    while true
+        D′ = min.(1, D^2)
+        D′ == D && break
+        D = D′
+    end
+    cnf = build_cnf!(X, [[0], [0]])
+    for (r, v) in zip(findn(P*D)...)
+        pkg_map[v] == r && continue
+        r in req_map[v] && continue
+        cnf[1][1], cnf[2][1] = v, -r
+        println((v, r, versions[v], packages[r]))
+        sat = PicoSAT.solve(cnf)
+        sat != :unsatisfiable && continue
+        println("REQ")
+        push!(req_map[v], r)
+    end
+    foreach(sort!, req_map)
+    return req_map
+end
+
+function pairwise_satisfiability(X::AbstractMatrix, D::AbstractMatrix=Main.D)
+    n = checksquare(X)
+    S = zeros(Bool, n, n)
+    cnf = build_cnf!(X, [[0], [0]])
+    p = sortperm(vec(sum(D, 1)), rev=true)
+    for a = 1:n-1, b = a+1:n
+        i, j = p[a], p[b]
+        X[i,j] == 0 || continue
+        S[i,j] == 0 || continue
+        println((a, b, versions[i], versions[j]))
+        cnf[1][1], cnf[2][1] = i, j
+        sat = PicoSAT.solve(cnf)
+        sat == :unsatisfiable && continue
+        vers = filter(x -> 1 <= x <= n, sat::Vector{Int})
+        @assert iszero(X[vers, vers])
+        @assert is_satisfied(vers)
+        while true
+            absent = (1:m)\(pkg_map[k] for k in vers)
+            satisfied = find(iszero, sum(R[absent,:],1))\vers
+            compsat = satisfied[find(iszero, sum(X[vers,satisfied],1))]
+            isempty(compsat) && break
+            sums = sum(S[:,vers], 2)
+            sort!(compsat, by = k -> sums[k])
+            for k in compsat
+                iszero(X[vers,k]) && push!(vers, k)
+            end
+        end
+        sort!(vers)
+        @assert iszero(X[vers, vers])
+        @assert is_satisfied(vers)
+        S[vers,vers] = 1
+        println("SAT: +", length(vers), ", ", 100*countnz(S)/length(S), "%")
+    end
+    return S
+end
+
+# if every compatible version of a requirement requires
+# something then make the top version require it too
+
+function propagate_requires!(req_map, X::AbstractMatrix = spzeros(Int,n,n))
+    # v: a package version
+    # req: a required package of v
+    # r: a version of req
+    # req_req: a requirement of r
+    # req_reqs: count requirements of r
+    req_reqs = Vector{Int}(m)
+    dirty = collect(1:n)
+    while !isempty(dirty)
+        vers = sort(dirty)
+        println("DIRTY: ", length(vers))
+        empty!(dirty)
+        for v in vers
+            println(versions[v])
+            for req in req_map[v]
+                req_vers = filter(x->X[v,x] == 0, pkg_vers[req])
+                isempty(req_vers) && continue
+                req_reqs .= 0
+                for r in req_vers
+                    for req_req in req_map[r]
+                        req_reqs[req_req] += 1
+                    end
+                end
+                l = length(req_vers)
+                for req_req in find(c->c >= l, req_reqs)
+                    if !(req_req in req_map[v])
+                        push!(req_map[v], req_req)
+                        append!(dirty, setdiff(req_rev[pkg_map[v]], dirty))
+                    end
+                end
+            end
+        end
+    end
+    foreach(sort!, req_map)
+end
+
+function propagate_conflicts!(X)
+    # v: a package version
+    # req: a required package of v
+    # r: a version of req
+    dirty = collect(1:n)
+    while !isempty(dirty)
+        vers = sort(dirty)
+        println("DIRTY: ", length(vers))
+        empty!(dirty)
+        for v in vers
+            println(versions[v])
+            for req in req_map[v]
+                conflicts = spzeros(Int, n)
+                for r in pkg_vers[req]
+                    conflicts += X[:,r]
+                end
+                l = length(pkg_vers[req])
+                x = find(c->c == l, conflicts)
+                if any(X[x,v] .== 0)
+                    X[x,v] = X[v,x] = 1
+                    append!(dirty, setdiff(req_rev[pkg_map[v]], dirty))
+                end
+            end
+        end
+    end
+end
+
+if false
+P = sparse(pkg_map, 1:n, 1, m, n)
+R = let p = [(i, j) for (j, v) in enumerate(req_map) for i in v]
+    sparse(first.(p), last.(p), 1, m, n)
+end
+X1 = incompatibility_matrix()
+propagate_requires!(req_map, X1)
+propagate_conflicts!(X1)
+@assert issymmetric(X1)
+
+D = iterate_dependencies(X1, P, R)
+Dp = min.(1, D*P')
+
+# S = pairwise_satisfiability(X)
+S = open(deserialize, "tmp/S.jls")
+X = sparse(ind2sub(size(S), find(iszero, S))..., 1)
+propagate_requires!(req_map, X)
+R = let p = [(i, j) for (j, v) in enumerate(req_map) for i in v]
+    sparse(first.(p), last.(p), 1, m, n)
+end
+D1 = max.(0, P'R .- X)
+D = iterate_dependencies(X1, P, R)
+Dp = min.(1, D*P')
+end
+
+include("graphutils.jl")
+
+# G :: 2^(m+n, m+n)
+# p, q ∈ 1:m     (packages)
+# v, w ∈ m+(1:n) (versions)
+# G[p, q] = 0 (no edges between packages)
+# G[p, v] = 1 ⟺ v is a version of p
+# G[v, p] = 1 ⟺ v requires p
+# G[v, w] = 1 ⟺ v and w are incompatible
+
+function sorttree!(T::StrongModuleTree{Int})
+    sort!(T, by=first_leaf)
+    sort!(T, by=leaf_count)
+    sort!(T, by=node_count)
+    return T
+end
+
+if false
+    TX = sorttree!(StrongModuleTree(X))
+    VX = versions[TX]
+
+    TD1 = sorttree!(StrongModuleTree(D1))
+    VD1 = versions[TD1]
+
+    TD = sorttree!(StrongModuleTree(D))
+    VD = versions[TD]
+
+    G = [spzeros(Int, m, m) P; R' X]
+    TG = sorttree!(StrongModuleTree(G))
+    VG = [packages; versions][TG]
+
+    H = [spzeros(Int, m, m) P; spzeros(Int, n, m) X]
+    TH = sorttree!(StrongModuleTree(H))
+    VH = [packages; versions][TH]
+
+    Y = X + D1
+    TY = sorttree!(StrongModuleTree(Y))
+    VY = versions[TY]
+end
+
+if false
+    x = find(Dp[:,packages_rev["Colors"]])
+    y = find(sum(P[:,x],2))
+    Px = P[y,x]
+    Rx = R[y,x]
+    X1x = X1[x,x]
+    pyvx = [packages[y]; versions[x]]
+
+    Xx = ones(Int, length(x), length(x))
+    for mis in maximal_indepedent_sets(X1, x)
+        z = findin(mis, x)
+        Xx[z,z] = 0
+    end
+    D1x = max.(0, Px'Rx .- Xx)
+
+    Gx = [spzeros(Int, length(y), length(y)) Px; Rx' Xx]
+    TG = sorttree!(StrongModuleTree(Gx))
+    VG = pyvx[TG]
+end
+
+# D1 .= max.(0, D1 .- X)
+# D .= max.(0, D .- X)
+
+# @assert iszero(D1 .& D1') # no mutual dependencies
+# @assert iszero(D1 .& X) # explict conflicts can't satisfy requirements
+# const G = D1 + X
+# @assert G .& G' == X
+# @assert max.(0, G .- G') == D1

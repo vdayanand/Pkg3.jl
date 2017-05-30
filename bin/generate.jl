7385a7e311733f929e1e029f9437729bcd648d7e
Stefan Karpinski <stefan@karpinski.org>
2017-05-30 17:57:57 -0400

    wip

--- bin/generate.jl
+++ bin/generate.jl
@@ -11,9 +11,9 @@ end
 toml_key(str::String) = ismatch(r"[^\w-]", str) ? repr(str) : str
 toml_key(strs::String...) = join(map(toml_key, [strs...]), '.')
 
-prefix = joinpath("tmp", "packages")
+prefix = joinpath("registry")
 
-write_toml(prefix, "index") do io, names
+write_toml(prefix, "packages") do io, names
     for (pkg, p) in sort!(collect(pkgs), by=(p->p.uuid.value)∘last)
         println(io, p.uuid, " = ", repr(pkg))
     end
@@ -27,13 +27,15 @@ end
 
 for (bucket, b_pkgs) in buckets
     for (pkg, p) in b_pkgs
-        write_toml(prefix, "metadata", bucket, pkg) do io, names
+        write_toml(prefix, bucket, pkg, "package") do io, names
             println(io, "name = ", repr(pkg))
             println(io, "uuid = ", repr(string(p.uuid)))
             println(io, "repo = ", repr(p.url))
-            for (ver, v) in sort!(collect(p.versions), by=first)
-                println(io)
-                println(io, "[", toml_key("version", string(ver)), "]")
+        end
+        write_toml(prefix, bucket, pkg, "versions") do io, names
+            for (i, (ver, v)) in enumerate(sort!(collect(p.versions), by=first))
+                i > 1 && println(io)
+                println(io, "[", toml_key(string(ver)), "]")
                 println(io, "hash-sha1 = ", repr(v.sha1))
             end
         end

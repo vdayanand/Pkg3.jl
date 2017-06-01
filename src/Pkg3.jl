98d538ea120d0bc0503e9fd19f36c74d4c4e610d
Stefan Karpinski <stefan@karpinski.org>
2017-06-01 09:33:51 -0400

    wip

--- src/Pkg3.jl
+++ src/Pkg3.jl
@@ -1,5 +1,12 @@
 module Pkg3
 
-# package code goes here
+struct Loader
+	env::Dict{String}
+	depot::String
+end
+
+function Base.load_hook(prefix::Loader, name::String, found)
+	
+end
 
 end # module

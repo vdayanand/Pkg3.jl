45c2d817e40cad211e08c5cda7675a258cef7f63
Stefan Karpinski <stefan@karpinski.org>
2017-06-13 10:10:35 -0400

    wip

--- src/Depots.jl
+++ src/Depots.jl
@@ -10,23 +10,25 @@ function registries(depot::String)
 	end
 end
 
-function _parse_packages(depot::String, registry::String)
+function parse_packages(depot::String, registry::String)
 	packages_toml = joinpath(depot, "registries", registry, "packages.toml")
 	packages = convert(Dict{String,String}, TOML.parsefile(packages_toml))
 end
 
 function package_name(depot::String, registry::String, uuid::String)
-	packages = _parse_packages(depot, registry)
-	packages[uuid] # throws KeyError => PackageNotFound(uuid)
+	packages = parse_packages(depot, registry)
+	get(packages, uuid, nothing)
 end
 
 function package_uuid(depot::String, registry::String, name::String)
-	packages = Dict(n => u for (u, n) in _parse_packages(depot, registry))
-	packages[name] # throws KeyError => PackageNotFound(name)
+	packages = Dict(n => u for (u, n) in parse_packages(depot, registry))
+	get(packages, name, nothing)
 end
 
 function install(depot::String, name::String, version::VersionNumber)
-
+	for registry in registries(depot)
+		uuid = 
+	end
 end
 
 end # module

b439ca0cc84dd9806fd3a759ca99319fa09002ca
Stefan Karpinski <stefan@karpinski.org>
2017-06-14 16:42:04 -0400

    wip

--- src/Depots.jl
+++ src/Depots.jl
@@ -15,23 +15,14 @@ function parse_packages(depot::String, registry::String)
 	packages = convert(Dict{String,String}, TOML.parsefile(packages_toml))
 end
 
-function package_uuid_to_name(depot::String, registry::String, uuid::String)
-	packages = parse_packages(depot, registry)
-	get(packages, uuid, nothing)
-end
-
-function package_name_to_uuids(name::String, depot::String, registry::String)
+function install(pkg::String, ver::VersionNumber)
 	uuids = String[]
-	for (uuid, pkg) in parse_packages(depot, registry)
-		pkg == name && push!(uuids, uuid)
+	for depot in Base.Loading.DEPOTS, registry in registries(depot)
+		for (uuid, name) in parse_packages(depot, registry)
+			name == pkg && push!(uuids, uuid)
+		end
 	end
 	return uuids
 end
-
-# function install(depot::String, name::String, version::VersionNumber)
-# 	for registry in registries(depot)
-# 		uuid = 
-# 	end
-# end
 
 end # module
